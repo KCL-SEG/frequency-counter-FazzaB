0df14bcd9aa2cb6bc581919d44c444c420bd996b
Fazza Bajwa <fazza.bajwa@kcl.ac.uk>
2022-10-07 14:04:00 +0100

    Solution

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
+    items2 = []
+    items = list(map(str, items))
     # Your code goes here
+    for item in items:
+        if item not in items2:
+            items2.append(item)
+    for item in items2:
+        if item in items:
+            frequencies[item] = items.count(item)
     return frequencies
